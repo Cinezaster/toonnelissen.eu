ce8ee5f1e7954305592cc83c487295f9510da4a5
toon nelissen appsaloon <toon@appsaloon.be>
2020-10-02 18:12:10 +0200

    feat: stuck

---
+++ src/routes/experimental/index.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Experimental</title>
+</svelte:head>
+
+<h1>Experimental</h1>
---
+++ src/routes/interest-graph/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let interests;
+</script>
+
+<svelte:head>
+	<title>Interest graph</title>
+</svelte:head>
+
+<h1>Interest graph</h1>
+
+<ul>
+	{#each interests as interest}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel='prefetch' href='blog/{interest.slug}'>{interest.title}</a></li>
+	{/each}
+</ul>
